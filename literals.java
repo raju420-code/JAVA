4e9fc3b4e752d493799fee00d74d8faf5c0f9836
unknown <sableraju1111@gmail.com>
2026-05-24 14:29:21 +0530

    first commit

---
+++ literals.java
@@ -0,0 +1,19 @@
+public class literals
+{
+    public static void main(String a[])
+    {
+        // Literals
+        int num1 = 0b101;
+        int num2 = 0x7E;
+        int num3 = 10_00_00_000;
+        double num4 = 55e17;
+        char c = 'w';
+        c++;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(c);
+    }
+
+}
