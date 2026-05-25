366e9a436e2334f09749f59ac5a4f0ceab17acfa
unknown <sableraju1111@gmail.com>
2026-05-25 16:44:49 +0530

    first commit

---
+++ operations.java
@@ -0,0 +1,15 @@
+public class operations 
+{
+    public static void main(String arg[])
+    {
+        int a = 3;
+        int b = 6;
+
+        double x = 5.6;
+        double y = 9.4;
+
+        boolean result = b>a && y>x;      // {>=,<=,==,!=,>,<}
+                                   // { &&-AND , ||-OR , !-NOT }
+        System.out.println(result);
+    }   
+}
